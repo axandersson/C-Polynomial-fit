99ec24552d41fc40525b14525e9073dbc40012b2
axandersson <axel.andersson78@gmail.com>
2018-06-14 11:54:43 +0200

    Made a class

--- PolyFit.cpp
+++ PolyFit.cpp
@@ -1,15 +1,12 @@
 #include "PolyFit.hpp"
 
-using namespace std;
-/* INPUT: A - vector of vector, corresponding to a square matrix having dimension N
- *        Tol - small tolerance number to detect failure when the matrix is near degenerate
- * OUTPUT: Matrix A is changed, it contains both matrices L-E and U as A=(L-E)+U such that P*A=L*U.
- *        The permutation matrix is not stored as a matrix, but in an integer vector P of size N+1 
- *        containing column indexes where the permutation matrix has "1". The last element P[N]=S+N, 
- *        where S is the number of row exchanges needed for determinant computation, det(P)=(-1)^S    
- * SOURCE: https://en.wikipedia.org/wiki/LU_decomposition
- */
-int LUPDecompose(vector<vector<double> >& A, const unsigned int& N,const double& Tol, vector<unsigned int> &P)
+
+PolyFit::SATUS PolyFit::LUPDecompose(
+	vector<vector<double> >& A,
+	const unsigned int& N,
+	const double& Tol,
+	vector<unsigned int> &P
+)
 {
 	unsigned int i, j, k, imax;
 	double maxA, absA;
@@ -29,7 +26,7 @@ int LUPDecompose(vector<vector<double> >& A, const unsigned int& N,const double&
 				imax = k;
 			}
 
-		if (maxA < Tol) return 0; //failure, matrix is degenerate
+		if (maxA < Tol) return SATUS::ERROR; //failure, matrix is degenerate
 
 		if (imax != i) 
 		{
@@ -49,20 +46,21 @@ int LUPDecompose(vector<vector<double> >& A, const unsigned int& N,const double&
 				A[j][k] -= A[j][i] * A[i][k];
 		}
 	}
-	return 1;  //decomposition done 
+	return SATUS::SUCCESS;  //decomposition done 
 }
 
-/* Solves the linear system Ax=b, using LUP decomposition.
- * INPUT: The square matrix A, of size N, and vector b.
- * OUTPUT: Vector x, the solution of Ax = b.
- * SOURCE: https://en.wikipedia.org/wiki/LU_decomposition
- */
-int LUPSolve(vector<double>& x, vector<vector<double> >& A, const vector<double>& b,const unsigned int& N)
+
+PolyFit::SATUS PolyFit::LUPSolve(
+	vector<double>& x,
+	vector<vector<double> >& A,
+	const vector<double>& b,
+	const unsigned int& N
+)
 {
 	const double tol = 1e-12;
 	vector<unsigned int> P(N + 1,0);
 
-	if(LUPDecompose(A, N, tol, P))
+	if(LUPDecompose(A, N, tol, P) == SATUS::SUCCESS)
 	{
 		for (unsigned int i = 0; i < N; i++) 
 		{
@@ -82,37 +80,18 @@ int LUPSolve(vector<double>& x, vector<vector<double> >& A, const vector<double>
 	}
 	else
 	{
-		// return 1 if matrix is degenerate,
-		// fit fails.
-		return 1;
+
+		return PolyFit::SATUS::ERROR;
 	}
-	return 0;
+	return PolyFit::SATUS::SUCCESS;
 }
 
-/*
- * Given a set of 2D points, stored in vectors x and y, 
- * finds polynomial coeficients of a polynomial of order 
- * pol_order that minimizes the sum of square distances,
- * using LUP-decomposition.
- *
- * INPUT: 
- *	- 	Vectors x and y, of equal length corresponding 
- *			to a 2D dataset. 
- *	- 	Integer pol_order corresponding to the desired order
- *	 		of the polynomial.
- * OUTPUT: 
- *	-	A vector poly_coef, of length pol_order+1, 
- * 			with estimated polynomial coeficients. The 
- *			coeficients are ordered as: 
- *			poly_coef[0] + poly_coef[1] x + ...
- *			 + poly_coef[pol_order] x^pol_order.
- * NOTES:
- *	-	Prints message if matrix is close to degenerate
- */
-int polyFit(vector<double>& outPolCoef, 
+PolyFit::SATUS PolyFit::polyFit(
+	vector<double>& outPolCoef, 
     const vector<double>& inX,
 	const vector<double>& inY, 
-	const unsigned int& inPolOrder)
+	const unsigned int& inPolOrder
+)
 {
 	const unsigned int nX = inX.size();
 	const unsigned int nCoef = inPolOrder + 1;
@@ -151,7 +130,6 @@ int polyFit(vector<double>& outPolCoef,
 		}
 	}
 
-	const int errCode = LUPSolve(outPolCoef,XX, XY, nCoef);
-	
+	const SATUS errCode = LUPSolve(outPolCoef,XX, XY, nCoef);
 	return errCode;
 }
--- test.cpp
+++ test.cpp
@@ -4,13 +4,12 @@
 using namespace std;
 int main() {
     const int order = 3;
-    vector<double> x = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0};
-    vector<double> y = {-7.5740, 5.2055, 11.3065, 9.4567, 7.0056, 1.2991, 20.3567, 52.3649, 105.1265, 196.0492};
+    const vector<double> x = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0};
+    const vector<double> y = {-7.5740, 5.2055, 11.3065, 9.4567, 7.0056, 1.2991, 20.3567, 52.3649, 105.1265, 196.0492};
     vector<double> polCoef;
     
-    const int error = polyFit(polCoef,x,y,order);
-
-    if(!error)
+    const PolyFit::SATUS ec = PolyFit::polyFit(polCoef,x,y,order);
+    if(ec == PolyFit::SATUS::SUCCESS)
     {
         cout << "Pol coeficient (c_0 + c_1 * x + c_2 x^2 + ... + c_n * x^n" << endl;
         cout << "c = [  ";
